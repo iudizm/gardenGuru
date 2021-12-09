7f1c79a333e70374cdc945311a10500d332d9e99
iudizm <iudizm@gmail.com>
2021-12-09 17:42:01 -0300

    Switch database adapter to `PostreSQL`

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_11_26_015401) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "plants", force: :cascade do |t|
     t.string "name"
     t.string "scientific_name"
@@ -20,4 +24,5 @@ ActiveRecord::Schema.define(version: 2021_11_26_015401) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
 end
